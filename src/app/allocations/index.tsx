d1b6ff31555515a8e6ca46970d1a5fa8e474a4c4
Nguyễn Thư <anhthunguyen7890@gmail.com>
2024-09-22 13:01:07 +0700

    Loi cho cai bang vi mat nut add

--- src/app/allocations/index.tsx
+++ src/app/allocations/index.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link, Stack } from 'expo-router';
 
-export default function App() {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Stack.Screen options={{title: 'Allocations'}}/>
+
+      <Text>Allocations</Text>
+
+      <Link href="/allocations/new">New Allocation</Link>
       <StatusBar style="auto" />
     </View>
   );
