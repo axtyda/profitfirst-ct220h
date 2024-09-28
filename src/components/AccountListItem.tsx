6e8e23c45e66c0af00aacaeaad7965af9dfd3922
axtyda <khoahienzaaxty@gmail.com>
2024-09-29 02:25:44 +0700

    delete account

--- src/components/AccountListItem.tsx
+++ src/components/AccountListItem.tsx
@@ -1,17 +1,27 @@
 import { View, Text, StyleSheet } from 'react-native';
 import Account from '../model/Account';
 import { withObservables } from '@nozbe/watermelondb/react'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import database from '../db';
 
 type AccountListItem = {
   account: Account;
 };
 
 function AccountListItem({ account }: AccountListItem) {
+
+  const onDelete = async () => {
+    await database.write(async () => {
+      await account.markAsDeleted();
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.name}>{account.name}</Text>
       <Text style={styles.percentage}>{account.cap}%</Text>
       <Text style={styles.percentage}>{account.tap}%</Text>
+      <AntDesign name="delete" size={18} color="gray" onPress={onDelete} />
     </View>
   );
 }
@@ -27,7 +37,7 @@ export default enhance(AccountListItem);
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    padding: 2,
+    padding: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
     borderRadius: 5,
@@ -35,6 +45,9 @@ const styles = StyleSheet.create({
   name: {
     fontWeight: 'bold',
     fontSize: 16,
+    flex: 1,
   },
-  percentage: {},
+  percentage: {
+    flex: 1,
+  },
 });
